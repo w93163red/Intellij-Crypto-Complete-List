5b39b0288c4ec1d2b39d053483b498bdbc6f67e9
Lingxiang LinG Wang <ling@DESKTOP-Q2CR683.localdomain>
2022-03-21 09:56:21 -0400

    get source code

--- src/main/java/com/example/completeplugin/ExtendCompletionContributor.java
+++ src/main/java/com/example/completeplugin/ExtendCompletionContributor.java
@@ -4,6 +4,7 @@ import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.compiler.chainsSearch.completion.MethodChainCompletionContributor;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PatternCondition;
 import com.intellij.patterns.PlatformPatterns;
@@ -13,12 +14,11 @@ import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
+import static com.intellij.patterns.PlatformPatterns.psiFile;
 import static com.intellij.patterns.PsiJavaPatterns.psiReferenceExpression;
 import static com.intellij.patterns.StandardPatterns.or;
 
 public class ExtendCompletionContributor extends CompletionContributor {
-    private static final Logger LOG = Logger.getInstance(ExtendCompletionContributor.class);
-
     public ExtendCompletionContributor() {
         ElementPattern<PsiElement> pattern = or(patternForMethodCallArgument(), patternForVariableAssignment(), patternForReturnExpression());
         extend(CompletionType.BASIC, pattern,
@@ -27,13 +27,14 @@ public class ExtendCompletionContributor extends CompletionContributor {
                     protected void addCompletions(@NotNull CompletionParameters parameters,
                                                   @NotNull ProcessingContext context,
                                                   @NotNull CompletionResultSet result) {
-                        System.out.println(123);
+                        System.out.println(parameters.getEditor().getDocument().getText());
                         result.addElement(LookupElementBuilder.create("hello"));
                     }
                 }
         );
     }
 
+    
     @NotNull
     private static ElementPattern<PsiElement> patternForMethodCallArgument() {
         return psiElement().withSuperParent(3, PsiMethodCallExpression.class).withParent(psiReferenceExpression().with(
